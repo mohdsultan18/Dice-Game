7a203a78a53bd7befb55f5c15dfb98d5fa752147
Mohd Sultan <sultan.mohd2023@gmail.com>
2024-02-10 15:41:48 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
+
+function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGameplay = () => {
+    setIsGameStarted((prev) => !prev);
+};
+
+  return (
+  <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGameplay} />}</>
+  );
+}
+
+export default App;
+
+
